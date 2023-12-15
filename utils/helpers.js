8cbf3283a0e8646413a27a7841237262d847a665
Daleray1231 <daleray1231@gmail.com>
2023-12-15 17:50:49 -0600

    added util helpers

---
+++ utils/helpers.js
@@ -0,0 +1,10 @@
+module.exports = {
+    format_date: (date) => {
+      if (date instanceof Date && !isNaN(date)) {
+        return date.toLocaleDateString();
+      } else {
+        console.error('Invalid date provided to format_date helper:', date);
+        return 'Invalid date'; 
+      }
+    },
+  };
