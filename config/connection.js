0a899afeb69f4df0c9f6172902f95b4230e0dc74
Daleray1231 <daleray1231@gmail.com>
2023-12-18 12:54:07 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
         {
             host: 'localhost',
             dialect: 'mysql',
-            password: '',
+            password: 'Karah712!',
             port: 3306
         }
     );
